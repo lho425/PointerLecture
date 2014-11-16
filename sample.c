5c8e07f2889c8f70a9f5de3de2e7ea41db07007e
lho425 <lho425@gmail.com>
2014-11-16 09:58:35 +0900

    Update sample.c

--- sample.c
+++ sample.c
@@ -2,28 +2,29 @@
 
 
 void main(){
-    char c = 0x07;
-    char *c_ptr = (char *)0xffffa000;
-	long long *lp;
+  char c = 0x07;
+  char *c_ptr = (char *)0xffffa000;
+  long long *lp;
 
-	unsigned int i;
+  unsigned int i;
 
-    printf("cのアドレス: %p\n",&c);
+  printf("cのアドレス: %p\n",&c);
 
-	
-	lp=&c;
-	*lp = 0xffffffffffff;
 
-	for(i=0;i<10000;i++){
-		*c_ptr=(char)i+0x21;//c_ptrの指すメモリ領域に値をブチ込む
-		c_ptr++;//一つ次の領域を指すようにする
-	}
-	
-	c_ptr = (char *)0xffffa000;
-	for(i=0;i<10000;i++){
-		*c_ptr=0;
-		c_ptr++;
-	}
-	
-	
+  lp=&c;
+  *lp = 0x00AABBCCDDEEFF;
+  printf("long longの大きさ: %d\n",sizeof(long long));
+
+  for(i=0;i<10000;i++){
+    *c_ptr=(char)i+0x21;//c_ptrの指すメモリ領域に値をブチ込む
+    c_ptr++;//一つ次の領域を指すようにする
+  }
+
+  c_ptr = (char *)0xffffa000;
+  for(i=0;i<10000;i++){
+    *c_ptr=0;
+    c_ptr++;
+  }
+
+
 }
